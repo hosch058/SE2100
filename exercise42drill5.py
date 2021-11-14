b62a234c6d44b1f725cc817016ed3da2783c6df3
hosch058 <94168705+hosch058@users.noreply.github.com>
2021-11-14 04:01:59 -0600

    Add files via upload

---
+++ exercise42drill5.py
@@ -0,0 +1,113 @@
+## Animal is-a object
+class Animal(object):
+    
+    def animalTest(self):
+        print("Animals are living members of the animal kingdom which we are very familiar with.")
+
+## dog is-a animal
+class Dog(Animal):
+
+    def __init__(self, name):
+        # dog has-a attribute 'name'
+        self.name = name
+
+    def dogName(self):
+        print(self.name)
+        print(f"Incessant barking from {self.name}")
+
+## cat is-a animal
+class Cat(Animal):
+
+    def __init__(self, name):
+        # cat has-a attribute 'name'
+        self.name = name
+
+    def catName(self):
+        print(self.name)
+
+## person is-a object
+class Person(object):
+
+    def __init__(self, name):
+        # Person has-a attribute(s) name and pet
+        self.name = name
+        self.pet = None
+
+    def hasPet(self):
+        if self.pet != None:
+            print(f"Yay, {self.name} has a pet whose name is {self.pet.name}")
+        else:
+            print(self.name, "is lonely and has no pet.")
+
+## employee is-a person
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        super(Employee, self).__init__(name)
+        # employee has-a attribute 'salary'
+        self.salary = salary
+    
+    def wastedLife(self):
+        print(f"{self.name} wastes the best years of their life only to make {self.salary} a year and in the end will just have a few bad years at the end of their life.")
+
+## fish is-a object
+class Fish(object):
+
+    def fishFact(self):
+        print("Fish have gills instead of lungs and live in the water. Their version of drowning is being on land. Ironic.")
+
+# salmon is-a fish
+class Salmon(Fish):
+
+    def salmonFact(self):
+        print("Salmon must run a gauntlet to get to their breeding grounds because bears love to eat them.")
+
+# halibut is-a fish
+class Halibut(Fish):
+
+    def halubutFact(self):
+        print("Halibut is flat fish that people supposedly like to eat. Idk what it tastes like.")
+
+
+# rover is-a dog
+rover = Dog("Rover")
+
+# satan is-definitely-a cat
+satan = Cat("Satan")
+
+daisy = Dog("Daisy")
+
+# Mary is-a person
+mary = Person("Mary")
+
+# Frank is-a(n) employee (who makes 120000)
+frank = Employee("Frank", 120000)
+
+# frank's pet is rover
+frank.pet = rover
+frank.pet = daisy
+
+mary.pet = satan
+
+# flipper is-a fish
+flipper = Fish()
+
+# crouse is-a salmon
+crouse = Salmon()
+
+# harry is-a halibut
+harry = Halibut()
+
+frank.pets = ["Rover", "Daisy"]
+frank.pets2 = {"Alive" : ["Rover", "Daisy"], "Dead" : ["Old Faithful", "Buddy"]}
+
+print("Don't mind me, just testing...", frank.pets)
+
+for x, y in frank.pets2.items():
+
+    if x == "Alive":
+        print(y, "are", x, "hooray! :D")
+    elif x == "Dead":
+        print(y, "have sadly passed on and are", x, "T.T")
+    else:
+        print("idk what you're trying to do")
